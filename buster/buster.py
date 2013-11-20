6da2772ae86bf105b8e1801bd3165ac15d2659e6
elyase <yaser.martinez@gmail.com>
2013-11-20 15:19:27 +0100

    make links relative for better offline viewing

--- buster/buster.py
+++ buster/buster.py
@@ -38,6 +38,7 @@ def main():
     if arguments['generate']:
         command = ("wget \\"
                    "--recursive \\"             # follow links to download entire site
+                   "--convert-links \\"         # make links relative
                    "--page-requisites \\"       # grab everything: css / inlined images
                    "--no-parent \\"             # don't go to parent level
                    "--directory-prefix {1} \\"  # download contents to static/ folder
